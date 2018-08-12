120751fa4b33c1dc998e9ba0282ac9f9a9c26cab
Jazzzscott <jadotscott@hotmail.com>
2018-08-12 11:27:34 -0700

    Completed Question One

--- CrackingTheCodingInterview/src/chapter_one/One_IsUnique.java
+++ CrackingTheCodingInterview/src/chapter_one/One_IsUnique.java
@@ -1,4 +1,7 @@
-package com.jasmineascott.ctci.chapterone;
+package chapter_one;
+
+import java.util.HashSet;
+import java.util.Set;
 
 /**
  * Implement an algorithm to determine if a string has all unique characters.
@@ -11,10 +14,17 @@ public class One_IsUnique {
 	//uses a set to hold unique values
 	public static boolean isUniqueA(String str1) {
 		
+		Set<Character> charSet = new HashSet<Character>();
+		char[] charArray = str1.toCharArray();
+		
 		//iterate through the char array
-		//insert each char into the set
+		for (char c : charArray) {
+			//insert each char into the set
+			charSet.add(c);
+		}
 		
 		//if the set length and the string length are equal then it is unique
+		if (charSet.size() == str1.length()) return true;
 		
 		return false;
 		
@@ -24,13 +34,17 @@ public class One_IsUnique {
 	//assumes that ASCII values are ok
 	public static boolean isUniqueB(String str1) {
 
-		//iterate through the char array for the string
-		//use an int array to increment the count at each index for the ASCII associated with each char
+		char[] charArray = str1.toCharArray();
+		int[] tracker = new int[256];
 		
-		//iterate throught the int array
-		//if any of the values is greater than one then the string is not unique 
-		
-		return false;
+		//iterate through string
+		for (int i = 0; i < str1.length(); i++) {
+			//if value at ASCII index is greater than 0, the character existed before so it is not unique
+			if (tracker[str1.charAt(i)] > 0) return false;
+			// else just increment the value for the first time
+			tracker[str1.charAt(i)]++;
+		}
+		return true;
 	}
 	
 	public static void main (String[] args) {
@@ -40,6 +54,7 @@ public class One_IsUnique {
 		for (String word : words ) {
 			System.out.println(word + " : " + isUniqueA(word));
 			System.out.println(word + " : " + isUniqueB(word));
+			System.out.println();
 		}
 	}
 }
--- CrackingTheCodingInterview/src/chapter_one/Two_CheckPermutation.java
+++ CrackingTheCodingInterview/src/chapter_one/Two_CheckPermutation.java
@@ -1,4 +1,4 @@
-package com.jasmineascott.ctci.chapterone;
+package chapter_one;
 
 /**
  * Given two strings, write a method to decide if one is a permutation of the other
