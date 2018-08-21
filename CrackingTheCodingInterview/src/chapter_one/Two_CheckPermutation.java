804c935adb9990c805377e173c9c1fa2d7b558cd
Jazzzscott <jadotscott@hotmail.com>
2018-08-20 20:42:01 -0700

    Complete Questions 2 and 3

--- CrackingTheCodingInterview/src/chapter_one/Two_CheckPermutation.java
+++ CrackingTheCodingInterview/src/chapter_one/Two_CheckPermutation.java
@@ -1,51 +1,74 @@
 package chapter_one;
 
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Given two strings, write a method to decide if one is a permutation of the other
  * TOPS, POTS, STPO, are all permutations of STOP
  * STOP has 4! permutations 
  * 		or 4 x 3 x 2 x 1 = 24 permutations
  * @author Jasmine
- * @version 8/12/2018
+ * @version 8/20/2018
  */
 public class Two_CheckPermutation {
 
+	//utilizes extra memory by using a Map
 	public static boolean isPermutationA(String str1, String str2) {
 		
+		Map<Character, Integer> permMap = new HashMap<Character, Integer>();
+		
 		//base case is to check that str1 length is equal to str2 length
+		if (str1.length() != str2.length()) return false;
 		
 		//iterate through str1
-		//if char has not been added to map insert char and a count of 1
-		//if char has been previously added to map increment count
+		for (char c : str1.toCharArray()) {
+			//if char has not been added to map insert char and a count of 1
+			if (!permMap.containsKey(c)) permMap.put(c, 1);
+			//if char has been previously added to map increment count
+			else if (permMap.containsKey(c)) permMap.put(c, permMap.get(c) +1);
+		}
 		
 		//iterate through str2
-		//if char has not been added to map insert char and a count of 1
-		//if char has been previously added to map decrement count
-		
+		for (char c : str2.toCharArray()) {
+			//if char has not been added to map then it is not a permutation
+			if (!permMap.containsKey(c)) return false;
+			//if char has been previously added to map decrement count
+			else if (permMap.containsKey(c)) permMap.put(c, permMap.get(c) -1);
+		}
+
 		//if any value in the map is greater than 0 the words are not permutations of each other
+		for (Character value : permMap.keySet()) {
+			if (permMap.get(value) != 0) return false;
+		}
 		
-		return false;
+		return true;
 	}
 	
-	//if assuming that chars are ASCII
+	//without utilizing extra memory
 	public static boolean isPermutationB(String str1, String str2) {
-		
-		//base case is to check that str1 length is equal to str2 length
-		
-		//iterate through str1
-		//increment the count at the index for each char's ASCII value into an array
-		
-		//Iterate through str2
-		//Decrement the count at the index for each char's ASCII value into an array
 
-		//if any value in the array is not equal to zero then the strings are not permutations
+		//base case is to check that str1 length is equal to str2 length
+		if (str1.length() != str2.length()) return false;
+				
+		//Sort the strings alphabetically
+		String temp1 = sortString(str1);
+		String temp2 = sortString(str2);
 		
-		return false;
+		//return true if the strings are equal or false if not equal
+		return temp1.equals(temp2);
+	}
+	
+	public static String sortString (String str) {
+		char[] temp = str.toCharArray();
+		Arrays.sort(temp);
+		return new String(temp);
 	}
 	
 	public static void main(String[] args) {
 		String word1 = "pots";
-		String word2 = "stop";
-		System.out.println(word1 + ", " + word2 + " : " + isPermutationA(word1, word2));
+		String word2 = "stol";
+		System.out.println(word1 + ", " + word2 + " : " + isPermutationB(word1, word2));
 	}
 }
